2f6a6c09fdb5f6afd766d22300c0cba416f0dd19
Robert <111635376+BigBadWolfPL@users.noreply.github.com>
2025-05-06 21:22:04 +0200

    Update roman_to_int.py

--- roman_to_int.py
+++ roman_to_int.py
@@ -1,7 +1,7 @@
 s = "MCMXCIV" # Output: 1994 "MCMXCIV"
 s_2 = "LVIII"
 s_3 = "III"
-s_4 ="MCDLXXVI" # 1476
+s_4 ="MCDLXXVI" # 1476     M CD L X X V I
 """
 Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 """
@@ -9,7 +9,6 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
 
 def romanToInt(s: str) -> int:
     length = len(s)
-    print(f"L = {length}")
     result = 0
     control = []
     roman_values = {
@@ -18,23 +17,40 @@ def romanToInt(s: str) -> int:
     'X': 10,   'IX': 9,   'V': 5,   'IV': 4,
     'I': 1
     }
+    #for i, j in zip(range(length), range(2, length)):
+    #    print(f"i: {i}, j: {j}")
+    idx = 0
     for i in range(length-1, -1, -2):
-        value = roman_values.get(s[i-1:i+1])
+        i += idx
+        part = s[i-1: i+1]
+        part_2 = s[i-2:i]
+        value = roman_values.get(part, 0)
+        #print(f"part: {part}, part_2: {part_2}")
         if value:
-            control.append(s[i-1:i+1])
+            print(f"part accepted: {part}")
             result += value
+            control.append(value)
+
+        elif roman_values.get(part_2, 0) :
+            print(f"part_2 accepted: {part_2}")
+            result += roman_values.get(part_2, 0)
+            result += roman_values.get(s[i], 0)
+            control.append(roman_values.get(part_2))
+            control.append(s[i])
+            idx -=1
+
         else:
-            for c in s[i-1:i+1]:
-                control.append(c)
-            result += sum([roman_values.get(x, 0) for x in s[i-1:i+1]])
-        if i == 2:
-            control.append(s[0])
-            result += roman_values.get(s[0], 0)
-    print(control)
-    print(result)
+            result += sum([roman_values.get(x, 0) for x in part])
+            for x in part:
+                control.append(x)
+        if not i and i+1 < 2:
+            result += roman_values.get(s[i], 0)
+            control.append(s[i])
+    print(control, result)
     return result
+        
 
 print(romanToInt(s) == 1994)
-#print(romanToInt(s_2) == 58)
-#print(romanToInt(s_3) == 3)
-print(romanToInt(s_4) == 1476)
+print(romanToInt(s_2) == 58)
+print(romanToInt(s_3) == 3)
+print(romanToInt(s_4) == 1476) 
