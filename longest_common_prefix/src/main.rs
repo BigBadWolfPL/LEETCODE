6ea0c8b202665bb2d0cf904121566c40eed2f17a
Robert <111635376+BigBadWolfPL@users.noreply.github.com>
2025-05-09 14:13:04 +0200

    Update main.rs

--- longest_common_prefix/src/main.rs
+++ longest_common_prefix/src/main.rs
@@ -11,27 +11,22 @@ struct Solution {}
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        if strs.len() == 0 {
+        if strs.is_empty() {
             return "".into();
         }
-        let strs_chars: Vec<Vec<char>> = strs
-            .iter()
-            .map(|x| x.chars().collect::<Vec<char>>())
-            .collect();
-
-        let min_length = strs_chars.iter().map(|x| x.len()).min().unwrap_or(0);
+        let first = strs[0].as_bytes();
+        let min_length = strs.iter().map(|w| w.len()).min().unwrap_or(0);
 
         for idx in 0..min_length {
-            for word in &strs_chars {
-                if strs_chars[0][idx] != word[idx] {
-                    return strs_chars[0][..idx].iter().collect();
+            for word in strs.iter() {
+                if first[idx] != word.as_bytes()[idx] {
+                    return String::from_utf8_lossy(&first[..idx]).into();
                 }
             }
         }
-        strs_chars[0][..min_length].iter().collect()
+        String::from_utf8_lossy(&first[..min_length]).into()
     }
 }
-
 
 /*
 fn main() {
