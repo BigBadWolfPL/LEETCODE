a0c6e37c6b8b49f830e6195a7c98ced5f9419c1a
robert <robertbielickiwet@gmail.com>
2025-05-13 22:42:33 +0200

    replace

--- total_sharacters_in_string_after_transformations_1/src/main.rs
+++ total_sharacters_in_string_after_transformations_1/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("{}", Solution::length_after_transformations("abcyy".to_string(), 2)); // 7
+    println!("WYNIK: {}", Solution::length_after_transformations("abcyy".to_string(), 2)); // 7
 }
 
 
@@ -9,14 +9,37 @@ impl Solution {
     pub fn length_after_transformations(s: String, t: i32) -> i32 {
         let mut result = s;
         for _ in 0..t {
-            result = result.replace("z", "ab");
-            result = result.replace("y", "z");
+        result = result
+            .replace("z", "ab")
+            .replace("y", "z")
+            .replace("x", "y")
+            .replace("w", "x")
+            .replace("v", "w")
+            .replace("u", "v")
+            .replace("t", "u")
+            .replace("s", "t")
+            .replace("r", "s")
+            .replace("q", "r")
+            .replace("p", "q")
+            .replace("o", "p")
+            .replace("n", "o")
+            .replace("m", "n")
+            .replace("l", "m")
+            .replace("k", "l")
+            .replace("j", "k")
+            .replace("i", "j")
+            .replace("h", "i")
+            .replace("g", "h")
+            .replace("f", "g")
+            .replace("e", "f")
+            .replace("d", "e")
+            .replace("c", "d")
+            .replace("b", "c")
+            .replace("a", "b");
         }
         result.len() as i32
     }
 }
-
-
 
 /*
 a => ab (26 operacji)              pozostało 
