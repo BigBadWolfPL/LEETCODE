2df2ff42da8fda002364fc8a1e8215fbc987de2d
Robert <111635376+BigBadWolfPL@users.noreply.github.com>
2025-05-23 09:07:35 +0200

    Add files via upload

--- Linked List/remove_nth_node_from_end.py
+++ Linked List/remove_nth_node_from_end.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+import unittest
 
 
 class ListNode:
@@ -21,11 +22,66 @@ class ListNode:
 test_node_1 = ListNode.build_node([1,2,3,4,5])
 
 
-def read_reverse(node: Optional[ListNode]) -> None:
+
+class Solution:
+    def removeNthFromEnd(head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        result = []
+        while head:
+            result.append(head.val)
+            head = head.next
+
+        if not result or len(result) == 1:
+            return None
+        result.pop(-n)
+        
+        new_head = ListNode(result[0])
+        current = new_head
+
+        for val in result[1:]:
+            current.next = ListNode(val)
+            current = current.next
+        return new_head
+
+
+'''
+TESTS
+'''
+
+class TestRemoveNthFromEnd(unittest.TestCase):
+    def test_val_in_list(self):
+        tested = Solution.removeNthFromEnd(ListNode.build_node([1,2,3,4,5]), 2)
+        expected = ListNode.build_node([1,2,3,5])
+
+        while tested or expected:
+            test_val = tested.val if tested else None
+            exp_val = expected.val if expected else None
+
+            self.assertEqual(test_val, exp_val, f"Difrent values: {test_val} != {exp_val}")
+            tested = tested.next if tested else None
+            expected = expected.next if expected else None
+
+
+    def test_one_element_one_remove(self):
+        tested = Solution.removeNthFromEnd(ListNode.build_node([1]), 1)
+        test_val = tested.val if tested else None
+        self.assertEqual(test_val, None, f"Result not None: {test_val}")
+
+if __name__ == "__main__":
+    unittest.main()
+
+"""
+
+def read_reverse(node: Optional[ListNode], c=0) -> None:
     if not node:
         return
-    read_reverse(node.next)
-    print(node.val)
+    c += 1
+    print(c, node.val)
+    read_reverse(node.next, c)
+    print(node.val, c)
 
 
 read_reverse(test_node_1)
+
+"""
+
+
